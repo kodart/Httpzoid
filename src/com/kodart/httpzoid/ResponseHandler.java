645b726dd86707d3da707fea0e42a23ba4035e3b
Victor ADĂSCĂLIȚEI <victor.adascalitei@tuscale.ro>
2014-07-02 21:46:05 +0200

    Added a synchronous "send" method to the HttpRequest interface and to the "HttpUrlonnectionRequest" implementation. Transformed the "ResponseHandler" into an interface so that it becomes easily to write "<Http instance>. [...] .handler(this).send()" which makes the code more readable.

--- src/com/kodart/httpzoid/ResponseHandler.java
+++ src/com/kodart/httpzoid/ResponseHandler.java
@@ -1,32 +1,32 @@
 package com.kodart.httpzoid;
 
 /**
- * Response callback handler
+ * Response callback handler interface
  * (c) Artur Sharipov
  */
-public class ResponseHandler<T> {
+public interface ResponseHandler<T> {
     /**
      * Notifies about success
      * @param data returned data
      * @param response http response object
      */
-    public void success(T data, HttpResponse response){}
+    void success(T data, HttpResponse response);
 
     /**
      * Notifies about error (http response code >= 400)
      * @param message error message
      * @param response http response object
      */
-    public void error(String message, HttpResponse response){}
+    void error(String message, HttpResponse response);
 
     /**
      * Notifies about network failure (offline, authentication error, etc.)
      * @param error
      */
-    public void failure(NetworkError error){}
+    void failure(NetworkError error);
 
     /**
      * Notifies about request complete (happens after success/error/failure)
      */
-    public void complete(){}
+    void complete();
 }
